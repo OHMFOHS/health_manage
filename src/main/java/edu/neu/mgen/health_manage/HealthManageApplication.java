5e7b18e0b05d9e5b69c89a5b8e5308effd8f4b52
ProgramMaster <programmaster@maojunchaodeMacBook-Pro.local>
2023-11-23 15:45:53 -0800

    Initial commit

---
+++ src/main/java/edu/neu/mgen/health_manage/HealthManageApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen.health_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/neu/mgen/health_manage/HealthManageApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.mgen.health_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
